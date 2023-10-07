fa353d53625b987a0b737bf42e4e9fa80fd94796
colin <colin.williams.dev@gmail.com>
2023-10-06 21:00:50 -0500

    Add project files.

---
+++ InventoryManager/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace InventoryManager
+{
+    partial class InventoryManagementSystemForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InventoryManagementSystemLabel = new System.Windows.Forms.Label();
+            this.inventoryGridView1 = new System.Windows.Forms.DataGridView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.costTextBox = new System.Windows.Forms.TextBox();
+            this.descTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.skuTextBox = new System.Windows.Forms.TextBox();
+            this.amtTextBox = new System.Windows.Forms.TextBox();
+            this.catComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InventoryManagementSystemLabel
+            // 
+            this.InventoryManagementSystemLabel.AutoSize = true;
+            this.InventoryManagementSystemLabel.Font = new System.Drawing.Font("Space Mono", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InventoryManagementSystemLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.InventoryManagementSystemLabel.Location = new System.Drawing.Point(107, 9);
+            this.InventoryManagementSystemLabel.Name = "InventoryManagementSystemLabel";
+            this.InventoryManagementSystemLabel.Size = new System.Drawing.Size(560, 48);
+            this.InventoryManagementSystemLabel.TabIndex = 0;
+            this.InventoryManagementSystemLabel.Text = "Inventory Management System";
+            // 
+            // inventoryGridView1
+            // 
+            this.inventoryGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.inventoryGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inventoryGridView1.Location = new System.Drawing.Point(12, 175);
+            this.inventoryGridView1.Name = "inventoryGridView1";
+            this.inventoryGridView1.Size = new System.Drawing.Size(776, 263);
+            this.inventoryGridView1.TabIndex = 1;
+            this.inventoryGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.inventoryGridView1_CellDoubleClick);
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Space Mono", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.addButton.Location = new System.Drawing.Point(12, 146);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(253, 23);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "ADD";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Font = new System.Drawing.Font("Space Mono", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteButton.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.deleteButton.Location = new System.Drawing.Point(530, 146);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(258, 23);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "DELETE";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Space Mono", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.saveButton.Location = new System.Drawing.Point(271, 146);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(253, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "SAVE";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // costTextBox
+            // 
+            this.costTextBox.Location = new System.Drawing.Point(12, 120);
+            this.costTextBox.Name = "costTextBox";
+            this.costTextBox.Size = new System.Drawing.Size(253, 20);
+            this.costTextBox.TabIndex = 5;
+            // 
+            // descTextBox
+            // 
+            this.descTextBox.Location = new System.Drawing.Point(271, 120);
+            this.descTextBox.Name = "descTextBox";
+            this.descTextBox.Size = new System.Drawing.Size(253, 20);
+            this.descTextBox.TabIndex = 6;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(271, 74);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(253, 20);
+            this.nameTextBox.TabIndex = 7;
+            // 
+            // skuTextBox
+            // 
+            this.skuTextBox.Location = new System.Drawing.Point(12, 74);
+            this.skuTextBox.Name = "skuTextBox";
+            this.skuTextBox.Size = new System.Drawing.Size(253, 20);
+            this.skuTextBox.TabIndex = 8;
+            // 
+            // amtTextBox
+            // 
+            this.amtTextBox.Location = new System.Drawing.Point(530, 120);
+            this.amtTextBox.Name = "amtTextBox";
+            this.amtTextBox.Size = new System.Drawing.Size(258, 20);
+            this.amtTextBox.TabIndex = 9;
+            // 
+            // catComboBox
+            // 
+            this.catComboBox.FormattingEnabled = true;
+            this.catComboBox.Location = new System.Drawing.Point(530, 74);
+            this.catComboBox.Name = "catComboBox";
+            this.catComboBox.Size = new System.Drawing.Size(258, 21);
+            this.catComboBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Space Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(13, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "SKU:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Space Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(272, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "NAME:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Space Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Info;
+            this.label3.Location = new System.Drawing.Point(530, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(258, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "CAT:";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Space Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Info;
+            this.label4.Location = new System.Drawing.Point(12, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(252, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "COST:";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Space Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Info;
+            this.label5.Location = new System.Drawing.Point(272, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(252, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "DESC:";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Space Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Info;
+            this.label6.Location = new System.Drawing.Point(530, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(258, 16);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "AMT:";
+            // 
+            // InventoryManagementSystemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.catComboBox);
+            this.Controls.Add(this.amtTextBox);
+            this.Controls.Add(this.skuTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.descTextBox);
+            this.Controls.Add(this.costTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.inventoryGridView1);
+            this.Controls.Add(this.InventoryManagementSystemLabel);
+            this.Name = "InventoryManagementSystemForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label InventoryManagementSystemLabel;
+        private System.Windows.Forms.DataGridView inventoryGridView1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox costTextBox;
+        private System.Windows.Forms.TextBox descTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox skuTextBox;
+        private System.Windows.Forms.TextBox amtTextBox;
+        private System.Windows.Forms.ComboBox catComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ InventoryManager/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManager
+{
+    public partial class InventoryManagementSystemForm : Form
+    {
+        DataTable inventory = new DataTable();
+
+        public InventoryManagementSystemForm()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inventory.Columns.Add("SKU");
+            inventory.Columns.Add("NAME");
+            inventory.Columns.Add("CAT");
+            inventory.Columns.Add("COST");
+            inventory.Columns.Add("DESC");
+            inventory.Columns.Add("AMT");
+
+            inventoryGridView1.DataSource = inventory;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inventoryGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
